6627b871484b66356472412b1a4fd3e4b977b268
Lucas Garcia e Silva <llucasgarciaesilva@hotmail.com>
2026-02-27 17:50:54 -0300

    build: criando e configurando o Stylecop.json para ajudar nas melhores práticas de código e atualizando o Directory.Build.props

--- AppProject.Core.Models.General/Platforms/Windows/PlatformClass1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AppProject.Core.Models.General;
-
-// All the code in this file is only included on Windows.
-public class PlatformClass1
-{
-}
--- AppProject.Core.Models.General/Platforms/iOS/PlatformClass1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AppProject.Core.Models.General;
-
-// All the code in this file is only included on iOS.
-public class PlatformClass1
-{
-}
